896ef56abe4c32e199a140a738b264c6b1ecb4ca
jfmam <jfmam@naver.com>
2023-05-10 01:24:14 +0900

    20230510

--- class4/1167.py
+++ class4/1167.py
@@ -1,33 +1,19 @@
-# 순서대로 먼저 어떻게 고를 것인가?
-#
 import sys
 input = sys.stdin.readline
+n, m = map(int, input().split())
+ch = [0] * (n+1)
+answer = []
 
-L, C = map(int, input().split())
-a = sorted(input().rstrip().split())
-ch = [0] * C
-answer = set([])
-
-
-def dfs(s, cnt, st):
-    global answer
-    if cnt == L:
-        j = 0
-        m = 0
-        for i in s:
-            if i in ['a', 'e', 'i', 'o', 'u']:
-                m += 1
-            else:
-                j += 1
-        if j >= 2 and m >= 1:
-            answer.add("".join(sorted(s)))
-    for i in range(st, C):
+def dfs(cnt, selected):
+    if cnt == m:
+        global answer
+        answer.append(selected[:])
+        return
+    for i in range(1, n+1):
         if ch[i] == 0:
             ch[i] = 1
-            dfs(s + a[i], cnt + 1, st + 1)
+            selected.append(i)
+            dfs(cnt+1, selected)
+            selected.pop()
             ch[i] = 0
-
-
-dfs('', 0, 0)
-for i in answer:
-    print(i)
+dfs(0,[])
