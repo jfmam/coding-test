88276e872937e8f31da9e9a18ba1c0bb712f6880
seung <49827304+jfmam@users.noreply.github.com>
2023-08-04 16:22:21 +0900

    Update 가장 긴 짝수 연속한 부분 수열 （large）.py

--- 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.py
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.py
@@ -1,33 +1,26 @@
-N, K = map(int, input().split())
-S = list(map(int, input().split()))
+n,k = map(int, input().split())
+a = list(map(int, input().split()))
 
-end = 0 # 끝 포인터
-result = 0 # 짝수로 이루어져 있는 연속한 부분 수열 중 가장 긴 길이(출력값)
-tmp = 0 # 현재 짝수 부분 수열의 길이
-count = 0 # 지우려는 홀수 카운트
+s,e = 0,0 # 끝
+result = 0 # 짝수로 이루어져 있는 연속한 부분 수열중 가장 긴길이
+even= 0 # s,e사이의 짝수 갯수
+odd = 0 # 지우려는 홀수 count 하기
 
-# start를 N 까지 계속 증가시키며 반복
-for start in range(N):
-    
-    # end를 가능한 만큼 이동
-    while (count <= K and end < N):     
+while s < n and e < n:
+    if odd > k:
+        result = max(result, even)
+        if a[s] % 2 == 1:
+            odd -= 1
+        else:
+            even -= 1
+        s += 1
+    else:
+        if a[e] % 2 == 1:
+            odd += 1
+        else:
+            even += 1
+        e += 1
         
-        if S[end] % 2 == 1: # 홀수
-            count += 1
-        else: # 짝수
-            tmp += 1
-        end += 1 # 끝 점 이동
-        
-        if start == 0 and end == N:
-            result = tmp
-            break
-        
-    if count == K+1 :
-        result = max(tmp, result)
-        
-    if S[start] %2 == 1: # 시작점이 홀수
-        count -= 1
-    else: # 시작점이 짝수
-        tmp -= 1
-        
+# even을 계속 더해주다가 k개를 초과하지 못하고 더하고 끝냈을 경우가 있기 때문에 한번 더 추가해준다.
+result = max(result, even)
 print(result)
