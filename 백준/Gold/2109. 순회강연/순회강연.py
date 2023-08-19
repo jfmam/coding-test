08fb35b3b1e71e544e8fd0bd3f0655c60ff40506
seung <49827304+jfmam@users.noreply.github.com>
2023-08-19 14:07:20 +0900

    [Gold III] Title: 순회강연, Time: 64 ms, Memory: 35372 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.py
+++ 백준/Gold/2109. 순회강연/순회강연.py
@@ -1,6 +1,5 @@
 import sys
-from collections import deque
-from collections import defaultdict
+import heapq
 input = sys.stdin.readline
 
 n = int(input())
@@ -8,17 +7,12 @@ a = []
 for _ in range(n):
     tmp = list(map(int, input().split()))
     a.append(tmp)
-a.sort(key=lambda x: (-x[0], x[1]))  # 먼저 점수 높은 것을 고려하도록 순서 변경
-q = deque(a)
-answer = 0
-ch = defaultdict(int)
+a.sort(key = lambda x: x[1]) # day를 기준으로 sort
+p_list = []
 
-while q:
-    p, d = q.popleft()
-    while ch[d]:  # 이미 해당 날짜에 선택된 경우에는 날짜를 감소시키며 빈 슬롯 찾기
-        d -= 1
-    if d >= 1:  # 선택된 날짜가 1 이상이어야 강연이 가능
-        answer += p
-        ch[d] = 1
-
-print(answer)
+for i in a:
+    p,d = i
+    heapq.heappush(p_list, p)
+    if(len(p_list) > d):
+        heapq.heappop(p_list)
+print(sum(p_list))
