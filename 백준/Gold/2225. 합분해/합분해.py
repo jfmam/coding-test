d6204a6760be61f58045ca65aba116fe4ddead32
seung <49827304+jfmam@users.noreply.github.com>
2023-08-24 10:39:12 +0900

    [Gold V] Title: 합분해, Time: 52 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -1,7 +1,11 @@
-n, k = map(int,input().split())
-dp = [[0] * (k+1) for _ in range(n+1)]
-dp[0][0] = 1
-for i in range(0, n+1):
-    for j in range(1, k+1):
-            dp[i][j] = dp[i-1][j] + dp[i][j-1]
-print(dp[n][k] % 1000000000)
+n,k = map(int, input().split()) # n은 수 k는 갯수
+d = [[0] * (k+1) for _ in range(n+1)]
+for i in range(1, n+1):
+    d[i][1] = 1
+for i in range(2, k+1):
+    d[1][i] = i
+for i in range(2,n+1):
+    for j in range(1,k+1):
+        d[i][j] = (d[i-1][j] + d[i][j-1]) % 1000000000
+print(d[n][k] % 1000000000)
+        
